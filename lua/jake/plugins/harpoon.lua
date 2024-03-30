7b7221b662d9d2e9c64d58cc39232d8bd79880d0
Jacob Strength <jstrength562@gmail.com>
2024-03-30 16:18:13 -0600

    add codeium plugin

--- lua/jake/plugins/harpoon.lua
+++ lua/jake/plugins/harpoon.lua
@@ -14,15 +14,14 @@ return {
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+        -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+        -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+        -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+        -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
-        -- need to remap this... why doesn't this work, even in tty??
-        vim.keymap.set("n", "<leader>bp", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<leader>bn", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
 
         -- basic telescope configuration
         local conf = require("telescope.config").values
